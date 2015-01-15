d45aa1257d297ef13da859ae023f35cb66d8165a
Michael <mikeybsaccount@gmail.com>
2015-01-15 23:24:02 +0000

    Form action changed

--- app/views/Url/Index.php
+++ app/views/Url/Index.php
@@ -3,7 +3,7 @@
   <span class="sr-only">Error:</span>
   (localhost) The site isn't fully functional yet. BUT we're working on it. Stay tuned!!
 </div>
-<form role="form" method="post" action="/kwnme/public/url">
+<form role="form" method="post" action="/">
 	<p style="float:right"><span class="special"></span><label for="url"></label><input type="text" name="url" value="Enter a URL and hit shorten :)" id="url" autocomplete="off" required="" onfocus="value='http://';"></p>
 	<p class="optional" style="float:right"><label for="slug">(optional) Custom Slug</label><input name="slug" value="" id="slug" autocomplete="off" placeholder="(i.e. shorty)" min="2" maxlength="32"></p>
 	<br style="clear:both">
