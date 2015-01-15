18f027dea54eb5593180ee4b9dd2de7db5220d75
Michael <mikeybsaccount@gmail.com>
2015-01-15 23:44:55 +0000

    css cleanup.

--- app/views/Url/Index.php
+++ app/views/Url/Index.php
@@ -1,7 +1,6 @@
-<div class="alert alert-danger" role="alert">
-  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-  <span class="sr-only">Error:</span>
-  (localhost) The site isn't fully functional yet. BUT we're working on it. Stay tuned!!
+<div class="alert alert-info" role="alert">
+	<i class="fa fa-bell-o"></i>
+	<strong>kwn.me</strong> is still being developed. We're now <a href="https://github.com/MadMikeyB/kwnme.framework.2.0" class="alert-link">open source!</a> Please report any bugs in our <a href="https://github.com/MadMikeyB/kwnme.framework.2.0/issues" class="alert-link">tracker</a>.
 </div>
 <form role="form" method="post" action="/">
 	<p style="float:right"><span class="special"></span><label for="url"></label><input type="text" name="url" value="Enter a URL and hit shorten :)" id="url" autocomplete="off" required="" onfocus="value='http://';"></p>
