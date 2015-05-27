1195788d562d7d14129bf692349c973615919109
Michael <mikeybsaccount@gmail.com>
2015-05-27 03:35:22 +0100

    Update Stats Links

--- app/views/Stats/Stats.php
+++ app/views/Stats/Stats.php
@@ -1,4 +1,4 @@
-<h2>Statistics &rarr; kwn.me/<?php echo ($data[1]->slug ? $data[1]->slug : $data[1]->base) ?></h2>
+<h2>Statistics &rarr; <a href="http://kwn.me/<?php echo ($data[1]->slug ? $data[1]->slug : $data[1]->base) ?>">kwn.me/<?php echo ($data[1]->slug ? $data[1]->slug : $data[1]->base) ?></a></h2>
 
 <ul class="list-group">
 	<li class="list-group-item"><span class="badge"><?php echo $data[1]->id ?></span> Unique ID</li>
